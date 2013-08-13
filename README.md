f60f92693794fef9674034ed1493da1a1d6e9624
Tongwu Zhang <xtmgah@gmail.com>
2013-08-12 22:58:33 -0400

    Update README

--- README.md
+++ README.md
@@ -1,4 +1,32 @@
 DDCV
 ====
 
-A Visualized Shiny App to Evaluate Drug-Drug Interaction
+A Visualized Shiny App to Evaluate Drug-Drug Interaction.
+
+Project status: **beta**
+
+Author: Tongwu Zhang (xtmgah AT gmail.com)
+
+
+Dependencies
+------------
+Tested with:
+* R
+
+* ggplot2
+
+* shiny
+
+* 
+
+
+Installation
+------------
+
+```bash
+git clone "https://github.com/xtmgah/DDCV.git"
+cd DDCV.git
+```
+
+![view](https://raw.github.com/xtmgah/DDCV/master/doc/rmprofile.pdf)
+
--- server.R
+++ server.R
@@ -1,5 +1,22 @@
 library(shiny)
 
+## Function to check whether package is installed
+
+is.installed <- function(mypkg) {
+  tmp <- is.element(mypkg,installed.packages()[,1])
+  if(!tmp){ install.packages(mypkg)}
+}
+
+
+## check if package is installed 
+is.installed("shiny")
+is.installed("ggplot2")
+is.installed("gplots")
+is.installed("reshape2")
+is.installed("lattice")
+is.installed("plyr")
+is.installed("scatterplot3d")
+
 sfiles<-list.files(path="DDCV_function/",pattern=".R",full.names=TRUE)
 for( fileA in sfiles) {source(fileA)}
 
