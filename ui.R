8c5509e00d23ce4ad8935e249e6a837b97e705ba
Tongwu Zhang <xtmgah@gmail.com>
2015-02-12 15:58:06 -0500

    Update panel doc
    Update panel doc

--- ui.R
+++ ui.R
@@ -85,7 +85,7 @@ shinyUI(fluidPage(theme=shinytheme("united"),
                  p("Quantitative analysis of dose-effect relationships: the combined effects of multiple drugs or enzyme inhibitors, advances in Enzyme Regulation 1984,22:27-55"),
                  
                  
-                 img(src="Theory.pdf",height=612, width=792)
+                 img(src="Theory.png",height=612, width=792)
         
                  ),
         tabPanel("Data",
@@ -115,19 +115,19 @@ shinyUI(fluidPage(theme=shinytheme("united"),
                  plotOutput("meffect2"),
                  br(),
                  br(),
-                 img(src="Equivalent.pdf",height=66,width=404)
+                 img(src="Equivalent.png",height=66,width=404)
                  ),
         
         tabPanel("Isobologram",
                  br(),
                  p("Show the drug-drug synergy/antagonism and interaction strength of each drug combination dose. Compared to traditional isobologram, DDCV isobologram have a good visualization of all tested drug combination dose."),
-                 img(src="Additivity.pdf",height=330,width=561),
+                 img(src="Additivity.png",height=330,width=561),
                  plotOutput("isob")),
         tabPanel("cIndex",
                  br(),
                  h4("Combination index (CI)"),
-                 img(src="CI2.pdf",height=260,width=435),
-                 img(src="CI.pdf",height=72,width=144),
+                 img(src="CI2.png",height=260,width=435),
+                 img(src="CI.png",height=72,width=144),
                  br(),
                  tags$ul(
                    tags$li("Calculate CI value in all drug combination dose."),
@@ -145,7 +145,7 @@ shinyUI(fluidPage(theme=shinytheme("united"),
                  br(),
                  p("Curve shift analysis allows simultaneous presentation of the studied concentration-effect curves of singe-agent and combination treatments in a single plot."),
                  br(),
-                 img(src="cshift.pdf",height=285,width=442),
+                 img(src="cshift.png",height=285,width=442),
                  tags$ul(
                    tags$li("Curve-shift based on two single drug and all drug combination.")),
                  plotOutput("cshift"),
@@ -156,13 +156,13 @@ shinyUI(fluidPage(theme=shinytheme("united"),
         tabPanel("Response",
                  br(),
                  p("This method assumes that the concentration-effect relationship for each drug separately follows the Hill equation and is designed to simultaneously fit all combination data to a single function."),
-                 img(src="URS.pdf",height=92,width=620),
+                 img(src="URS.png",height=92,width=620),
                  plotOutput("usresponse"),
                  plotOutput("usresponse2")),
         tabPanel("Contour",
                  br(),
                  p("The contour plot can be used to guide next drug combination experiment for specific effect."),
-                 img(src="contour.pdf",height=359,width=434),
+                 img(src="contour.png",height=359,width=434),
                  plotOutput("dcontour")),
         tabPanel("Document",
                  includeHTML("README.html"))
